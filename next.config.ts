b1a33e70770b26afb3ac16739b272d167dd99dd3
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2025-08-07 14:09:13 +0530

    feat: Add ESLint configuration file and enable ignoreDuringBuilds option in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors. Be sure to fix lint errors in dev!
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
